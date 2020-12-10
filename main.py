15d38cfcfed5b41d990cb9c3a37293a9300d31ca
vidun-jay <vidunvj@gmail.com>
2020-12-10 12:54:43 -0500

    Pretty much initial commit

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
 
 while True:
+    cap.set(cv2.CAP_PROP_FPS, 60)
     _, img = cap.read()
-
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = cascade.detectMultiScale(gray, 1.1, 4)
 
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 4)
 
-    cv2.imshow('img', img)
-    cv2.waitKey(30)
+    cv2.imshow('Facial Recognition', img)
+    cv2.moveWindow('Facial Recognition', 60, 0)
+
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
 
 cap.release()
